6d9ec8c4726aaa625443110beb5b90c2f4c644fc
卢运烽 <414675087@qq.com>
2017-01-08 17:08:55 +0800

    tint drawable in java code

---
+++ app/src/main/java/com/luyunfeng/tintselector/MainActivity.java
@@ -0,0 +1,21 @@
+package com.luyunfeng.tintselector;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Drawable drawable = Util.getCheckTintSelector(this, "sort_distance_default", R.color.normal_color_selector);
+        drawable.setBounds(0,0,50,50);
+
+        CheckBox cb_test = (CheckBox) findViewById(R.id.cb_test);
+        cb_test.setCompoundDrawables(drawable, null, null, null);
+    }
+}
---
+++ app/src/main/java/com/luyunfeng/tintselector/Util.java
@@ -0,0 +1,53 @@
+package com.luyunfeng.tintselector;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringDef;
+import android.support.v4.graphics.drawable.DrawableCompat;
+
+/**
+ * Created by luyunfeng on 2017/1/8.
+ */
+
+public class Util {
+
+    public static Drawable getCheckTintSelector(Context context, @DrawableRes int drawableId, @ColorRes int color) {
+
+        Resources resources = context.getResources();
+
+        Drawable drawable = resources.getDrawable(drawableId);
+
+        ColorStateList colorList = resources.getColorStateList(
+                color
+        );
+
+        return tintDrawable(drawable, colorList);
+    }
+
+    public static Drawable getCheckTintSelector(Context context, String function, @ColorRes int color) {
+
+        int defaultId = context.getResources().getIdentifier(function, "drawable", BuildConfig.APPLICATION_ID);
+
+        if (defaultId == 0) {
+            defaultId = R.drawable.ic_launcher;
+        }
+
+        return getCheckTintSelector(context, defaultId, color);
+    }
+
+    private static Drawable tintDrawable(Drawable drawable, ColorStateList colors) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            drawable.setTintList(colors);
+            return drawable;
+        }else{
+            final Drawable wrappedDrawable = DrawableCompat.wrap(drawable);
+            DrawableCompat.setTintList(wrappedDrawable, colors);
+            return wrappedDrawable;
+        }
+    }
+}
